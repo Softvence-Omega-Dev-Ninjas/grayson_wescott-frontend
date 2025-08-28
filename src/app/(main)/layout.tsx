45274fbb9285f06bb55833efa7cde63845565f71
Arnab Gupta <arnabgupta1111@gmail.com>
2025-08-28 16:49:44 +0600

    fixed

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,16 +1,15 @@
-
-import Footer from "@/components/shared/main/footer/Footer";
-import Header from "@/components/shared/main/header/Header";
+import Footer from "@/components/shared/main/Footer/Footer";
+import Header from "@/components/shared/main/Header/Header";
 import React, { ReactNode } from "react";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
-  return <div>
-    <Header/>
-    {children}
-    <Footer/>
-    </div>;
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
-
-
